79bbe8826fd222a7ae0df94cb5412089829e2ead
HarkonenBade <github@harkonen.net>
2020-03-31 15:05:15 +0100

    Use the proper owner check

--- cogs/management.py
+++ cogs/management.py
@@ -1,13 +1,8 @@
 from discord.ext import commands
 
-def can_manage():
-    def pred(ctx: commands.Context):
-        return ctx.bot.is_owner(ctx.author)
-    return commands.check(pred)
-
 class Manage(commands.Cog):
     @commands.command(hidden=True)
-    @can_manage()
+    @commands.is_owner()
     async def restart(self, ctx: commands.Context):
         await ctx.send(f"Ok **{ctx.author}**\n*SYSTEM RESTARTING*")
         ctx.bot.loop.stop()
